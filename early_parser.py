3d45aa06fcead162d196f1aad85ce0cc11df8c24
Otávio Jacobi <otaviojacobi@gmail.com>
2016-05-12 22:31:58 -0300

    printando a saida mais bonita

--- early_parser.py
+++ early_parser.py
@@ -311,6 +311,17 @@ def earley_parser(frase, inicial, terminais, regras, variaveis):
 
     return D #retorna a lista de hashs
 
+def limpa_arvores(D):
+    for dicionario in D:
+        for chave in dicionario.keys():
+            for lista in dicionario[chave]:
+                for i in range(len(lista)):
+                    if '(' in lista[i]:
+                        lista[i] = lista[i].replace('@', '').replace('/', '').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','')
+                        lista[i] = lista[i].replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
+
+    return D
+
 def ver_se_parsed(inicial, D):
     '''
     String, Lista de Hash de Lista de Lista -> Bool
@@ -366,7 +377,7 @@ def main():
             #print "D%d"%i
             #printStuff(D[i],'r')
         print "A SENTENÇA PERTENCE A GRAMÁTICA !!!"
-        #D=exclui_brindes(D)
+        D=limpa_arvores(D)
         save_D(D, saida)
         #print D[7]
     else:
